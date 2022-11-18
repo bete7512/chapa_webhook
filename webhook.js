0d8387b1b8fb3c5286ba6163a4a99033c40ae09c
bete7512 <betekbebe@gmail.com>
2022-11-18 12:07:00 +0300

    special

--- webhook.js
+++ webhook.js
@@ -3,16 +3,17 @@ var express = require('express');
 var secret = process.env.SECRET_KEY;
 // Using Express
 var app = express();
-app.get('/', (req,res)=>{
-res.send("well")
+app.get('/', (req, res) => {
+    res.send("well")
 })
-app.post("/event/webhook", function(req, res) {
+app.post("/event/webhook", function (req, res) {
     //validate event
     const hash = crypto.createHmac('sha256', secret).update(JSON.stringify(req.body)).digest('hex');
     if (hash == req.headers['Chapa-Signature']) {
-    // Retrieve the request's body
-    const event = req.body;
-    // Do something with event  
+        // Retrieve the request's body
+        const event = req.body;
+        // Do something with event  
     }
     res.send(200);
 });
+app.listen(4000, () => console.log('Listening on port 3000!'))
