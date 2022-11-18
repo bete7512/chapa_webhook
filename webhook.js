2c98dabaf413cebafbb4b743f08ba8ebad6aa7b2
bete7512 <betekbebe@gmail.com>
2022-11-18 14:39:40 +0300

    special

--- webhook.js
+++ webhook.js
@@ -1,21 +1,31 @@
 var crypto = require('crypto');
 var express = require('express');
-// var secret = process.env.SECRET_KEY;
+var secret = 'MIIDDTCCAfWgAwIBAgIJJACYXtGC0O6GMA0GCSqGSIb3DQEBCwUAMCQxIjAgBgNV';
 // Using Express
 var app = express();
+app.use(express.json());
 app.get('/', (req, res) => {
     res.send("well")
 })
-app.post("/event/webhook", function (req, res) {
+app.post("/event/webhook", async (req, res) => {
     //validate event
+    console.log(req.headers);
     console.log("from webhook", req.body);
-    console.log(req.body)
-    const hash = crypto.createHmac('sha256', 'MIIDDTCCAfWgAwIBAgIJJACYXtGC0O6GMA0GCSqGSIb3DQEBCwUAMCQxIjAgBgNV').update(JSON.stringify(req.body)).digest('hex');
-    if (hash == req.headers['Chapa-Signature']) {
+    // console.log(req.body)
+    // try {
+    const hash =  crypto.createHmac('sha256', secret).update(JSON.stringify(req.body)).digest('hex');
+    // console.log(hash);
+
+    if (hash == req.headers['x-chapa-signature']) {
         // Retrieve the request's body
-        const event = req.body;
+        // const event = req.body;
         // Do something with event  
+        return res.status(200).send('OK');
     }
-    res.send(200);
+    else {
+        return res.status(400).send('Invalid signature');
+    }
+    // res.send(200);
 });
 app.listen(4000, () => console.log('Listening on port 3000!'))
+// x-chapa-signature":"c6e20538cd830d2c1deef379c8d220c2ec5e52aa35ea7d064307e8eb90970806"
