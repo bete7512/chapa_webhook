c27ed55022afb802205e13d4766424b095e3e067
Bete Goshme <99134122+bete7512@users.noreply.github.com>
2022-11-18 13:56:14 +0300

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -1,6 +1,6 @@
 var crypto = require('crypto');
 var express = require('express');
-var secret = process.env.SECRET_KEY;
+// var secret = process.env.SECRET_KEY;
 // Using Express
 var app = express();
 app.get('/', (req, res) => {
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
 app.post("/event/webhook", function (req, res) {
     //validate event
     console.log(req.body)
-    const hash = crypto.createHmac('sha256', secret).update(JSON.stringify(req.body)).digest('hex');
+    const hash = crypto.createHmac('sha256', 'MIIDDTCCAfWgAwIBAgIJJACYXtGC0O6GMA0GCSqGSIb3DQEBCwUAMCQxIjAgBgNV').update(JSON.stringify(req.body)).digest('hex');
     if (hash == req.headers['Chapa-Signature']) {
         // Retrieve the request's body
         const event = req.body;
